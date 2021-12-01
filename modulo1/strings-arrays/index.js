a0c95785c2dc997c2c2a6f59f15dd05b3a5ddf1f
Jessica Oliveira <jessicaoliveira@Jessicas-Mac-mini.local>
2021-12-01 13:38:31 -0300

    exercícios de arrays e strings com desafios

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,76 @@
+//Exercícios de Interpretação de código
+//1.
+//a. undefined
+//b. null
+//c. 11
+//d. 3
+//e. [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+//f. 9
+
+//2.
+//Será impresso: SUBI NUM ÔNIBUS EM MIRROCOS 27
+
+//Exercícios de Escrita de Código
+
+//1.
+// const nomeUsuario = prompt("Qual o seu nome?")
+// const emailUsuario = prompt("Qual o seu endereço de email?")
+// console.log(`O email ${emailUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeUsuario}!`)
+
+//2.
+// let comidasPreferidas = ["Chocolate", "Churrasco", "Strogonoff", "Sorvete", "Pizza"]
+
+//a.
+// console.log(comidasPreferidas)
+
+//b.
+// console.log(`Essas são minhas comidas preferidas:\n ${comidasPreferidas[0]}\n ${comidasPreferidas[1]}\n ${comidasPreferidas[2]}\n ${comidasPreferidas[3]}\n ${comidasPreferidas[4]}`)
+
+//c.
+// let comidaPreferidaUsuario = prompt("Qual a sua comida preferida?")
+
+// comidasPreferidas[1] = comidaPreferidaUsuario
+
+// console.log(comidasPreferidas)
+
+//3.
+//a.
+// let listaDeTarefas = []
+
+//b.
+// const primeiraTarefa = prompt("Digite uma tarefa que você precisa fazer hoje.")
+
+// listaDeTarefas.push(primeiraTarefa)
+
+// const segundaTarefa = prompt("Digite mais uma tarefa que você precisa fazer.")
+
+// listaDeTarefas.push(segundaTarefa)
+
+// const terceiraTarefa = prompt("Digite a última tarefa que você precisa realizar hoje.")
+
+// listaDeTarefas.push(terceiraTarefa)
+
+//c.
+// console.log(listaDeTarefas)
+
+//d.
+// const tarefaRealizada = Number(prompt("Digite o número da tarefa que você já realizou de 0 a 2"))
+
+//e.
+// listaDeTarefas.splice(tarefaRealizada, 1)
+// console.log(listaDeTarefas)
+
+//Desafios
+
+//a.
+// const frase = [prompt("Digite uma frase aqui.")]
+
+// const listaPalavras = frase.split(" ")
+// console.log(listaPalavras)
+
+//b.
+// const arrayFrutas = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"]
+
+// console.log(arrayFrutas.indexOf("Abacaxi"), arrayFrutas.length)
+
+
