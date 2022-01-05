5c0e5394c4836ee336cf717e4c7f6a4d71265c30
Jessica Oliveira <jessicaoliveira@Jessicas-Mac-mini.local>
2022-01-04 23:08:29 -0300

    Lista de reforço JS a finalizar

--- modulo1/reforco-js-lista2/lista-revisao-js2/exercicios.js
+++ modulo1/reforco-js-lista2/lista-revisao-js2/exercicios.js
@@ -19,8 +19,8 @@ function retornaArrayOrdenado(array) {
 }
 
 // EXERCÍCIO 04
-function retornaNumerosPares(array) {
-  const arrayNumerosPares = array.filter((elemento) => {
+function retornaNumerosPares(array) { 
+  return array.filter((elemento) => {
       return elemento % 2 === 0
   })
 
@@ -28,12 +28,23 @@ function retornaNumerosPares(array) {
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
- 
+  return array.filter((elemento)=> {
+    return elemento % 2 === 0
+  }).map((elemento) => {
+    return elemento ** 2
+  })
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
+  let maiorNumero = 0
+
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] > maiorNumero) {
+      maiorNumero = array[i]
+    }
+  }
+  return maiorNumero
 }
 
 // EXERCÍCIO 07
@@ -48,7 +59,13 @@ function retornaNPrimeirosPares(n) {
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-
+  if (ladoA === ladoB && ladoB === ladoC){
+    return "Equilátero"
+  } else if (ladoA === ladoB || ladoB === ladoC || ladoA === ladoC) {
+    return "Isósceles"
+  } else if (ladoA !== ladoB && ladoB !== ladoC && ladoC !== ladoA) {
+    return "Escaleno"
+  }
 }
 
 // EXERCÍCIO 10
@@ -58,27 +75,43 @@ function retornaSegundoMaiorESegundoMenor(array) {
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+   const mensagemFilme = `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores[0]}, ${filme.atores[1]}, ${filme.atores[2]}, ${filme.atores[3]}.`
+
+   return mensagemFilme
 }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+   const pessoaAnonima = {
+    ...pessoa,
+    nome: "ANÔNIMO"
+   }
+   return pessoaAnonima
+
 }
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   
+   const pessoasAutorizadas = pessoas.filter(pessoa => {
+     return pessoa.idade > 14 && 
+     pessoa.idade < 60 &&
+     pessoa.altura > 1.5
+   })
+   return pessoasAutorizadas
 }
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
-  
+  const pessoasNaoAutorizadas = pessoas.filter(pessoa => {
+    return pessoa.idade <= 14 ||
+    pessoa.idade > 60 ||
+    pessoa.altura < 1.5
+  })
+  return pessoasNaoAutorizadas
 }
 
 // EXERCÍCIO 14
 function retornaContasComSaldoAtualizado(contas) {
-
 }
 
 // EXERCÍCIO 15A
