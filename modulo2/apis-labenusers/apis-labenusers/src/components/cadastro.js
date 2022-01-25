8fe995934af6c58c60b82fcf4713b9a20d11aaf5
Jessica Oliveira <jessicaoliveira@Jessicas-Mac-mini.local>
2022-01-25 17:02:26 -0300

    função getAllUsers

--- modulo2/apis-labenusers/apis-labenusers/src/components/cadastro.js
+++ modulo2/apis-labenusers/apis-labenusers/src/components/cadastro.js
@@ -4,9 +4,9 @@ import React from "react";
 
 export default class Cadastro extends React.Component {
   state = {
-    users: {},
-    name: "",
-    email: "",
+    inputName: "",
+    inputEmail: "",
+    list: [],
   };
 
   componentDidMount() {
@@ -21,6 +21,30 @@ export default class Cadastro extends React.Component {
     this.setState({ email: event.target.value });
   };
 
+  createUser = () => {
+    const urlCreateUsers =
+      "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users";
+    const axiosConfig = {
+      headers: { Authorization: "jessica-oliveira-vaughan" },
+    };
+    const body = { name: this.state.inputName, email: this.state.inputEmail };
+
+    axios
+      .post(urlCreateUsers, body, axiosConfig)
+      .then((response) => {
+        alert("Usuário cadastrado com sucesso!");
+        this.setState({ name: "" });
+        this.setState({ email: "" });
+        this.getAllUsers();
+      })
+      .catch((error) => {
+        // alert("Não foi possível cadastrar o usuário, tente novamente");
+        alert(error);
+        this.setState({ name: "" });
+        this.setState({ email: "" });
+      });
+  };
+
   getAllUsers = () => {
     const urlGetAllUsers =
       "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users";
@@ -29,47 +53,38 @@ export default class Cadastro extends React.Component {
     };
     axios
       .get(urlGetAllUsers, axiosConfig)
-      .then((response) => {})
-      .catch();
-  };
-
-  createUser = () => {
-    const urlCreateUsers =
-      "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users";
-    const axiosConfig = {
-      headers: { Authorization: "jessica-oliveira-vaughan" },
-    };
-    const body = { name: this.state.name, email: this.state.email };
-
-    axios
-      .post(urlCreateUsers, body, axiosConfig)
       .then((response) => {
-        alert("Usuário cadastrado com sucesso!");
-        console.log(response);
-        this.setState({ name: "" });
-        this.setState({ email: "" });
+        this.setState({ userList: response.data });
       })
       .catch((error) => {
-        alert("Não foi possível cadastrar o usuário");
-        console.log(error);
+        alert(error.response.data.message);
       });
   };
+
   render() {
+    const userList = this.state.list.map((user) => {
+      return (
+        <div>
+          <p>{user.name}</p>
+        </div>
+      );
+    });
+    console.log("estado: ", this.state);
     return (
       <div>
         <p>Cadastre seu usuário</p>
         <input
-          value={this.state.name}
+          value={this.state.inputName}
           onChange={this.handleName}
           placeholder="Nome"
         />
         <input
-          value={this.state.email}
+          value={this.state.inputEmail}
           onChange={this.handleEmail}
           placeholder="Email"
         />
         <button onClick={this.createUser}>Cadastrar usuário</button>
-        <button>Mudar de Tela</button>
+        <button onClick={this.getAllUsers}>Lista de Usuários</button>
       </div>
     );
   }
