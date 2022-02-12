1eb51603e6cc098a03d2117b2f698ab757a89d19
Jessica Oliveira <jessicaoliveira@Jessicas-Mac-mini.local>
2022-02-12 11:03:29 -0300

    voltando a estrutura inicial de páginas

--- modulo2/astromatch/astromatch/src/App.js
+++ modulo2/astromatch/astromatch/src/App.js
@@ -1,12 +1,32 @@
 import react, { useState } from "react";
 import styled from "styled-components";
 import axios from "axios";
-import Main from "./components/Main/Main";
+import HomePage from "./components/HomePage/HomePage";
+import MatchesPage from "./components/MatchesPage/MatchesPage";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("home-page");
+
+  const switchPage = () => {
+    switch (currentPage) {
+      case "home-page":
+        return <HomePage />;
+      case "matches-page":
+        return <MatchesPage />;
+    }
+  };
+
+  const goToHomePage = () => {
+    setCurrentPage("home-page");
+  };
+
+  const goToMatchesPage = () => {
+    setCurrentPage("matches-page");
+  };
+
   return (
     <>
-      <Main />
+      <div>{switchPage()}</div>
     </>
   );
 }
---
+++ modulo2/astromatch/astromatch/src/components/MatchesPage/MatchesPage.js
@@ -0,0 +1,11 @@
+import react, { useState, useEffect } from "react";
+import styled from "styled-components";
+import axios from "axios";
+
+export default function MatchesPage() {
+  return (
+    <>
+      <div>Matches Page</div>
+    </>
+  );
+}
