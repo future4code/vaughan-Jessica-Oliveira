5927c8249d51351cedb31686bc2535fa84ad1af1
Jessica Oliveira <jessicaoliveira@Jessicas-Mac-mini.local>
2022-04-09 15:29:55 -0300

    exercicio 1

---
+++ modulo4/node-package-json/index.js
@@ -0,0 +1,15 @@
+//Exercício 1
+
+//a. pelo comando process.argv
+
+//b.
+const nome = process.argv[2];
+const idade = Number(process.argv[3]);
+
+console.log(`Olá, ${nome}! Você tem ${idade} anos.`);
+
+console.log(
+  `Olá ${nome}! Você tem ${idade} anos. Em sete anos você terá ${
+    idade + Number(7)
+  } anos.`
+);
